a4c0be443ba8ee221d0a0b837432f6021500ba6b
Володимир <lionkr23@gmail.com>
2024-01-15 18:18:49 +0200

    New project

---
+++ main.py
@@ -0,0 +1,13 @@
+number = int(input('Input some value '))
+if number > 5:
+    print('You value is more than 5')
+elif number < 5:
+    print('You value is lese than 5')
+else:
+    print('You value is 5')
+
+number = int(input('Input some value '))#5....10
+if number > 5 or number < 10:
+    print('True')
+else:
+    print('False')
