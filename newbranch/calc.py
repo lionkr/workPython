28091a15b0a27457ddf03796c7cf69005486032b
Володимир <lionkr23@gmail.com>
2024-01-15 18:27:58 +0200

    newBranch file calc

---
+++ newbranch/calc.py
@@ -0,0 +1,11 @@
+from math_operations import *
+
+def calculator(c):
+    if c == '+':
+        return add.addition
+    elif c == '-':
+        return subt.substraction
+    elif c =='*':
+        return mult.multiplication
+    elif c =='/':
+        return div.divide
